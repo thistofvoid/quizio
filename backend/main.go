a465c46138d30bbc8f00cb324a4ab661dfd8ca49
blue3prints <278412660+thistofvoid@users.noreply.github.com>
2026-05-26 18:19:12 +0300

    Add pocketbase to go project

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,15 @@
 package backend
 
 import (
-	"fmt"
+	"log"
+
+	"github.com/pocketbase/pocketbase"
 )
 
 func main() {
-	fmt.Println("Hello WOrld")
+	app := pocketbase.New()
+
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
 }
